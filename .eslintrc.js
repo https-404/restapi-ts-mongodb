dc63b2fdc9ce7152d709953ca3ff9297c6d57241
areesh <areesh.192003@gmail.com>
2024-07-08 05:02:54 +0500

    Add initial project configuration files and dependencies

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+const { rules } = require("eslint-config-prettier");
+
+module.exports = {
+    parser: "@typescript-eslint/parser",
+    extends: [
+        "plugin:@typescript-eslint/recommended",
+        "prettier/@typescript-eslint",
+        "plugin:prettier/recommended"
+    ],
+    parserOptions: {
+        ecmaVersion: 2018,
+        sourceType: "module"
+    },
+    rules: {
+        
+    }
+}
